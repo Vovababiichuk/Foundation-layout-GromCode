f5045d472170d255f51d384b6cdc42241d7531df
VVVV BBBB <Volodymyr>
2024-06-06 17:31:23 +0300

    created feedback section

--- js/index.js
+++ js/index.js
@@ -18,3 +18,38 @@ try {
 } catch (e) {
 	console.log(e);
 }
+
+// swiper
+
+const swiper = new Swiper(".mySwiper", {
+	direction: "horizontal",
+	loop: true,
+	autoplay: {
+		delay: 1500,
+		disableOnInteraction: false,
+	},
+	pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+	},
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+	const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+	window.addEventListener("scroll", function() {
+			if (window.scrollY > 200) {
+					scrollToTopBtn.style.display = "block";
+			} else {
+					scrollToTopBtn.style.display = "none";
+			}
+	});
+
+	scrollToTopBtn.addEventListener("click", function() {
+			window.scrollTo({
+					top: 0,
+					behavior: "smooth"
+			});
+	});
+});
+
