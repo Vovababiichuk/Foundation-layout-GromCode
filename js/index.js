97ad11fa5f8055e6bf611cf27577360bc5f5f8b6
VVVV BBBB <Volodymyr>
2024-06-06 12:29:25 +0300

    added popup for header

--- js/index.js
+++ js/index.js
@@ -2,21 +2,19 @@ const dropdown = document.getElementById('library');
 const dropdownMenu = document.getElementById('dropdown');
 const overlay = document.getElementById('overlay');
 
-
 try {
-    dropdown.addEventListener('click', (event) => {
-        dropdownMenu.classList.toggle('show');
-				overlay.classList.toggle('overlay');
-        event.stopPropagation();
-    });
+	dropdown.addEventListener('click', event => {
+		dropdownMenu.classList.toggle('show');
+		overlay.classList.toggle('overlay');
+		event.stopPropagation();
+	});
 
-
-    document.addEventListener('click', (event) => {
-        if (!dropdownMenu.contains(event.target)) {
-						dropdownMenu.classList.remove('show');
-						overlay.classList.remove('overlay');
-        }
-    });
+	document.addEventListener('click', event => {
+		if (!dropdownMenu.contains(event.target)) {
+			dropdownMenu.classList.remove('show');
+			overlay.classList.remove('overlay');
+		}
+	});
 } catch (e) {
-    console.log(e);
+	console.log(e);
 }
